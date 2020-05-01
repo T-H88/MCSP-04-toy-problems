a14e017a3c918c78c8d0a905dd78fd39cb0ba246
Tyler Hicks <tylerhicks8088@gmail.com>
2020-05-01 09:55:26 -0500

    Finished

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -2,6 +2,15 @@
 
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
 
-function maxConsecutiveOnes(array) {
-  //write solution code
+function maxConsecutiveOnes(array){
+  var count = 0;
+  var max = 0;
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] == 0)
+      count = 0;
+    else
+      count++;
+    max = count > max ? count : max;
+  }
+  return max;
 }
