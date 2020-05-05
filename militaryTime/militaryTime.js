718e5bd7abe4b9de092c10ecc592ffc8a7465574
Tyler Hicks <tylerhicks8088@gmail.com>
2020-05-05 10:11:09 -0500

    Finished

--- militaryTime/militaryTime.js
+++ militaryTime/militaryTime.js
@@ -1,6 +1,16 @@
 // Write a function that takes in a string of the time and returns the time in military format.
 //Ex: militaryTime('7:47pm') returns '19:47'
 
+ //if hour is 0 then change to 12, if hour is 1200-2300 subtract 12.
+
 function militaryTime(timeStr) {
-  //write code here
+  const regularTime = '12:00 p.m'
+    let amPm = time.slice(-3)
+    let mil = ''
+
+    if (amPm === 'a.m') {
+      let arr = time.slice(0, -4)
 }
+};
+
+//Stuck right here. not sure how im going to divide the string up for time purposes. 
